0f4a1b925799cff7e94323edafd0041b6ea2089d
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2024-08-09 11:12:10 +0900

    cloud-init用のcustom_dataを追加と依存関係のデバッグ

--- infra/04_create_vm.bash
+++ infra/04_create_vm.bash
@@ -2,6 +2,30 @@
 
 source .env
 
+custom_data=$(cat << '__EOS__'
+#!/bin/bash
+
+echo "@@@ custom_data start @@@"
+while fuser /var/lib/dpkg/lock-frontend >/dev/null 2>&1 ; do
+  echo "Waiting for other apt-get instances to exit"
+  sleep 1
+done
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y ca-certificates curl jq
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+echo "@@@ custom_data end @@@"
+__EOS__
+)
+
 az deployment group create \
   --resource-group "${RESOURCE_GROUP_NAME}" \
   --template-file ./templates/04_vm.template.json \
@@ -11,11 +35,12 @@ az deployment group create \
       adminName="${VM_ADMIN}" \
       adminPublicKey="$(cat ${VM_ADMIN_PUBLIC_KEY_PATH})" \
       skuName="${VM_SKU}" \
-      osDiskSku="${VM_OSDISK_SKU}"
+      osDiskSku="${VM_OSDISK_SKU}" \
+      customData="${custom_data}"
 
 public_ip=$(az network public-ip show \
   --resource-group "${RESOURCE_GROUP_NAME}" \
-  --name "${PREFIX}-PublicIP" \
+  --name "${PREFIX}-VMIP" \
   --query "ipAddress" \
   --output tsv)
 
