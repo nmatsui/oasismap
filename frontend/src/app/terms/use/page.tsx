a88215029b696f79d6560def971c6e80b72252fb
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2024-10-31 09:21:20 +0900

    参加同意書のデバッグ

--- frontend/src/app/terms/use/page.tsx
+++ frontend/src/app/terms/use/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import { Grid, Typography, ListItemText } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 
 const TermsOfService: React.FC = () => {
   return (
@@ -12,14 +12,17 @@ const TermsOfService: React.FC = () => {
     >
       <Grid item xs={12} md={6}>
         <Grid item textAlign="center" sx={{ my: 2 }}>
-          <Typography variant="h5">（雛形）地域幸福度可視化アプリ オープンベータ版 実証実験参加同意事項</Typography>
+          <Typography variant="h5">
+            （雛形）地域幸福度可視化アプリ オープンベータ版 実証実験参加同意事項
+          </Typography>
         </Grid>
         <Grid item textAlign="right">
-          <Typography variant="body1">【自治体名】   </Typography>
+          <Typography variant="body1">【自治体名】 </Typography>
           <Typography variant="body1">yyyy年mm月dd日</Typography>
         </Grid>
         <Grid item textAlign="left" sx={{ my: 2 }}>
-        この度は、【自治体名】が実施する“地域幸福度可視化アプリ オープンベータ版”の実証実験にご協力いただき、誠にありがとうございます。本実証実験への参加にあたり、以下の内容をご確認のうえ、アカウント入力画面にチェックをお入れください。
+          この度は、【自治体名】が実施する“地域幸福度可視化アプリ
+          オープンベータ版”の実証実験にご協力いただき、誠にありがとうございます。本実証実験への参加にあたり、以下の内容をご確認のうえ、アカウント入力画面にチェックをお入れください。
           <Typography variant="subtitle1" sx={{ fontWeight: 'bold', mt: 2 }}>
             １．実証実験の概要
           </Typography>
@@ -30,15 +33,17 @@ const TermsOfService: React.FC = () => {
           <Typography variant="subtitle1" sx={{ fontWeight: 'bold', mt: 2 }}>
             ２．参加条件
           </Typography>
-          <Typography variant="body1" style={{ wordWrap: "break-word" }}>
-            ・インターネット接続可能なスマートフォンをお持ちの方<br/>
+          <Typography variant="body1" style={{ wordWrap: 'break-word' }}>
+            ・インターネット接続可能なスマートフォンをお持ちの方
+            <br />
             ・Googleアカウントをお持ちでログイン操作ができる方
           </Typography>
           <Typography variant="subtitle1" sx={{ fontWeight: 'bold', mt: 2 }}>
             ３．提供サービス
           </Typography>
           <Typography variant="body1">
-            ・地域幸福度可視化アプリ利用者アプリの提供<br/>
+            ・地域幸福度可視化アプリ利用者アプリの提供
+            <br />
             ・アプリ利用に関するサポート
           </Typography>
           <Typography variant="subtitle1" sx={{ fontWeight: 'bold', mt: 2 }}>
@@ -52,24 +57,29 @@ const TermsOfService: React.FC = () => {
             ５．禁止事項
           </Typography>
           <Typography variant="body1">
-            ・本サービスを利用した営業活動<br/>
-            ・TIS株式会社および【自治体名】による本サービスの運営を妨害する行為<br/>
+            ・本サービスを利用した営業活動
+            <br />
+            ・TIS株式会社および【自治体名】による本サービスの運営を妨害する行為
+            <br />
             ・他のユーザ、第三者の権利を侵害する行為
           </Typography>
           <Typography variant="subtitle1" sx={{ fontWeight: 'bold', mt: 2 }}>
             ６．健康管理と安全に関する注意事項
           </Typography>
           <Typography variant="body1">
-            ・活動を行う際には、ご自身の健康状態を十分に管理ください。<br/>
-            ・体調がすぐれない場合や、医師から運動を控えるように指示されている場合は、参加を控えてください。<br/>
-            ・適切な水分補給と休憩を心がけ、無理のない範囲で活動を行ってください。<br/>
+            ・活動を行う際には、ご自身の健康状態を十分に管理ください。
+            <br />
+            ・体調がすぐれない場合や、医師から運動を控えるように指示されている場合は、参加を控えてください。
+            <br />
+            ・適切な水分補給と休憩を心がけ、無理のない範囲で活動を行ってください。
+            <br />
             ・安全な場所で活動を行い、交通ルールの順守をお願いします。
           </Typography>
           <Typography variant="subtitle1" sx={{ fontWeight: 'bold', mt: 2 }}>
             ７．同意の撤回
           </Typography>
           <Typography variant="body1">
-          ・本実証実験への参加は任意であり、いつでも同意を撤回することができます。同意を撤回しても不利益を被ることはありません。
+            ・本実証実験への参加は任意であり、いつでも同意を撤回することができます。同意を撤回しても不利益を被ることはありません。
           </Typography>
         </Grid>
       </Grid>
