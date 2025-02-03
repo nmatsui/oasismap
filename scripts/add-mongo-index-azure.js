16ac92bfacc898adb935c6cd87eaee5deac74aad
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2025-02-03 15:19:24 +0900

    Azure cosmosdb mongodbにインデックスを作成する初期化コンテナを追加

---
+++ scripts/add-mongo-index-azure.js
@@ -0,0 +1,21 @@
+if (!db.getCollectionNames().includes("entities")) {
+  db.createCollection("entities");
+}
+db.entities.createIndex({
+  "_id.servicePath": 1,
+  "_id.id": 1,
+  "_id.type": 1
+});
+db.entities.createIndex({"creDate": 1});
+db.entities.createIndex({"_id.id": 1});
+db.entities.createIndex({"_id.type": 1});
+db.entities.createIndex({"_id.servicePath": 1});
+db.entities.createIndex({"attrNames": 1});
+db.entities.createIndex({"location.coords": "2dsphere"});
+// oasismap original
+db.entities.createIndex({"attrs.timestamp.value": 1});
+db.entities.createIndex({"attrs.nickname.value": 1});
+db.entities.createIndex({
+  "attrs.timestamp.value": 1,
+  "attrs.nickname.value": 1
+});
